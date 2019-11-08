65e1fc4c0d1cf814605ed93f424f86ed99a271fb
Alice Freda <alicefreda@gmail.com>
2019-11-08 22:11:28 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,10 @@
 class Cat
-  # code goes here
+  attr_reader :name
+  attr_accessor :owner, :mood
+  @@all = []
+  def initialize(name, owner_instance)
+  end
+  def self.all
+    @@all
+  end
 end
