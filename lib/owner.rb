cdc622efc42af595374d34327aadbeef7f68d40d
Alice Freda <alicefreda@gmail.com>
2019-11-08 22:31:13 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,11 +1,12 @@
 class Owner
   attr_reader :name, :species
   @@all = []
-  def initialize(species="human")
+  def initialize(name, species="human")
     @species = species
+    @name = name
   end
   def name
-
+    
   end
   # def species
   #
