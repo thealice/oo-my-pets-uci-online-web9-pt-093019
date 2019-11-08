2b5305edf79e1b8ac1a4f84d2cbf26ddf25aa3fa
Alice Freda <alicefreda@gmail.com>
2019-11-08 22:02:12 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,22 @@
 class Owner
-  # code goes here
+  def name
+
+  end
+  def species
+
+  end
+  def say_species
+
+  end
+  #Class methods
+  def self.all
+    
+  end
+  def self.count
+
+  end
+  def self.reset_all
+
+  end
+
 end
