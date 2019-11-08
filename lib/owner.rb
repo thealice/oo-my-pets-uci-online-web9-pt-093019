69cdb52b86fecf0f27f531625e8c64a7738cd060
Alice Freda <alicefreda@gmail.com>
2019-11-08 22:34:49 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,16 +1,12 @@
 class Owner
   attr_reader :name, :species
   @@all = []
+
   def initialize(name, species="human")
     @species = species
     @name = name
   end
-  # def name
-  #
-  # end
-  # def species
-  #
-  # end
+
   def say_species
     "I am a #{self.species}."
   end
@@ -18,36 +14,47 @@ class Owner
   def self.all
     @@all
   end
+
   def self.count
     @all.count
   end
+
   def self.reset_all
     @@all = []
   end
+
   def cats
 
   end
+
   def dogs
 
   end
+
   def buy_cat
 
   end
+
   def buy_dog
 
   end
+
   def walk_dogs
 
   end
+
   def feed_cats
 
   end
+
   def sell_cats
 
   end
+
   def sell_pets
 
   end
+  
   def list_pets
 
   end
