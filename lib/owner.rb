bb666ca3872ff8cdc0e794b3c3b2f2fdcf417d9c
Alice Freda <alicefreda@gmail.com>
2019-11-08 22:27:53 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -23,5 +23,32 @@ class Owner
   def self.reset_all
     @@all = []
   end
+  def cats
+
+  end
+  def dogs
+
+  end
+  def buy_cat
+
+  end
+  def buy_dog
+
+  end
+  def walk_dogs
+
+  end
+  def feed_cats
+
+  end
+  def sell_cats
+
+  end
+  def sell_pets
+
+  end
+  def list_pets
+
+  end
 
 end
